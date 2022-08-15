275ddfb105f326a98d6713aad2d247a7666b100b
GabeMagalhaes <gabriel.oliveira.magalhaes@outlook.com>
2022-08-14 22:51:49 -0300

    Fetching welcome screen style (work in progress)

--- ivet/lib/screens/welcome.dart
+++ ivet/lib/screens/welcome.dart
@@ -5,34 +5,44 @@ class WelcomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body:Center(
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              'images/welcomeimg.png',
-              height: 255.19,
-              width: 355.59,
-            ),
-            Text(
-              'iVet',
-              style: TextStyle(fontSize: 24)
-            ),
-            Text(
-              'Assuma o controle de saúde do seu pet e encontre uma clinica ao seu alcance'
-            ),
-            Divider(
-              height: 3,
-              indent: 50,
-              endIndent: 50,
-              color: Colors.black,
-            ),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/login'),
-              child: Text('Começe Já')
-              ),
-          ],
-        ),
-      ),
+      body: _buildContent()
     );
   }
+}
+
+
+Widget _buildContent(){
+  return Container(
+    // padding: EdgeInsets.all(5),
+    color: Color.fromARGB(255, 221, 228, 235),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Image.asset(
+          'images/welcomeimg.png',
+          height: 255.19,
+          width: 355.59,
+        ),
+        Container(
+          color: Colors.purple,
+          child: SizedBox(
+          height: 100,
+          ),
+        ),
+        Container(
+          color: Colors.orange,
+          child: SizedBox(
+          height: 100,
+          ),
+        ),
+        Container(
+          color: Colors.red,
+          child: SizedBox(
+          height: 100,
+          ),
+        ),
+      ],
+    ),
+  );
 }
