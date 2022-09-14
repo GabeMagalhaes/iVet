32cb017745d47aca0ea8bb8af220606d1278d18d
GabeMagalhaes <gabriel.oliveira.magalhaes@outlook.com>
2022-09-13 22:35:14 -0300

    spubkspeck and Runnes files updated

--- ivet/lib/screens/login.dart
+++ ivet/lib/screens/login.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatelessWidget {
   const LoginScreen();
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text('LOGIN'),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/home'),
-              child: Text('Login')
-            )
-          ],
-        )
-      )
-    );
+        body: Center(
+            child: Column(
+      children: <Widget>[
+        Text('LOGIN'),
+        ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/home'),
+            child: Text('Login'))
+      ],
+    )));
   }
 }
