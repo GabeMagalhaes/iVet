8f270be4d15dc27c1448ef6fd196d63ba506deca
GabeMagalhaes <gabriel.oliveira.magalhaes@outlook.com>
2022-09-29 20:35:44 -0300

    Data loading from firestore is working, fixed loading screen

--- ivet/lib/view/home.dart
+++ ivet/lib/view/home.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ivet/view/Consultas.dart';
 import 'package:ivet/view/atividade.dart';
@@ -32,96 +34,129 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 70,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-              icon: Icon(
-                Icons.border_all_rounded,
-                size: 32,
-                color: Colors.blue,
+
+    // Firestore instanciada
+    FirebaseFirestore fdb = FirebaseFirestore.instance;
+    // Collection 'Users' instanciada
+    CollectionReference users = fdb.collection('Users');
+    // UID do usuario (FireAuthenticator)
+    String userAuthUID = FirebaseAuth.instance.currentUser!.uid;
+    // String do ID do documento que se refere ao usuario (Firestore Database)
+    String userID = userAuthUID.toString();
+    // Referecia do Documento do usuario (Firestore Database)
+    DocumentReference<Map<String, dynamic>> userDocRef =fdb.collection('Users').doc(userID);
+    // Instanciando os Snapshots da coleçåo Users em uma Stream
+    final Stream<QuerySnapshot> _usersStream = users.snapshots();
+    final Stream<DocumentSnapshot<Map<String, dynamic>>> _userStream = userDocRef.snapshots();
+
+  
+    return StreamBuilder<DocumentSnapshot>(
+      stream: _userStream,
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+              color: Color.fromARGB(255, 238, 238, 238),
+            );
+        }
+        
+        String name = snapshot.data!['name'].toString();
+
+        return DefaultTabController(
+          length: 5,
+          child: Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 70,
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                  icon: Icon(
+                    Icons.border_all_rounded,
+                    size: 32,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () {}),
+              title: Text(
+                name, 
+               style: TextStyle(
+                  color: Colors.black
+                ),
               ),
-              onPressed: () {}),
-          title: Text(
-            "NOME DO USER",
-            style: TextStyle(
-              color: Colors.black
+              actions: [
+                IconButton(
+                    icon: Icon(
+                      Icons.notifications_none,
+                      size: 32,
+                      color: Colors.blue
+                    ),
+                    onPressed: () {})
+              ],
+              bottom: TabBar(
+                labelColor: Colors.black,
+                unselectedLabelColor: Color.fromARGB(255, 157, 157, 157),
+                tabs: [
+                  Tab(
+                    text:'Atividade',
+                  ),
+                  Tab(
+                    text: 'Clinicas',
+                  ),
+                  Tab(
+                    text: 'Histórico',
+                  ),
+                  Tab(
+                    text: 'Carteira de vacinação',
+                  ),
+                  Tab(
+                    text: 'Adoção',
+                  )
+                ],
+                isScrollable: true,
+              ),
+            ),
+
+            body: IndexedStack(
+              index: currentIndex,
+              children: screens,
+            ),
+
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.favorite,
+                size: 35,
+              ),
+              backgroundColor: Color.fromRGBO(255, 101, 132, 1),
+            ),
+            
+            bottomNavigationBar: BottomAppBar(
+              shape: CircularNotchedRectangle(),
+              elevation: 0,
+              child: BottomNavigationBar(
+                currentIndex: currentIndex,
+                iconSize: 40,
+                showUnselectedLabels: false,
+                backgroundColor: Colors.white,
+                selectedItemColor: Colors.blueAccent,
+                onTap: (index) => setState(() => currentIndex = index),
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Conta',
+                  )
+                ],
+              ),
             ),
           ),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.notifications_none,
-                  size: 32,
-                  color: Colors.blue
-                ),
-                onPressed: () {})
-          ],
-          bottom: TabBar(
-            labelColor: Colors.black,
-            unselectedLabelColor: Color.fromARGB(255, 157, 157, 157),
-            tabs: [
-              Tab(
-                text:'Atividade',
-              ),
-              Tab(
-                text: 'Clinicas',
-              ),
-              Tab(
-                text: 'Histórico',
-              ),
-              Tab(
-                text: 'Carteira de vacinação',
-              ),
-              Tab(
-                text: 'Adoção',
-              )
-            ],
-            isScrollable: true,
-          ),
-        ),
-
-        body: IndexedStack(
-          index: currentIndex,
-          children: screens,
-        ),
-
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
-            Icons.favorite,
-            size: 35,
-          ),
-          backgroundColor: Color.fromRGBO(255, 101, 132, 1),
-        ),
-        
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          elevation: 0,
-          child: BottomNavigationBar(
-            currentIndex: currentIndex,
-            iconSize: 40,
-            showUnselectedLabels: false,
-            backgroundColor: Colors.white,
-            selectedItemColor: Colors.blueAccent,
-            onTap: (index) => setState(() => currentIndex = index),
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Conta',
-              )
-            ],
-          ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
